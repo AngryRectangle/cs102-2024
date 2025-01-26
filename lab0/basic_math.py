d301b62e574ba6f68a34749fc544e0f64d9412d7
AngryRectangle <yashmel2000@gmail.com>
2025-01-26 13:31:29 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -2,38 +2,63 @@ import numpy as np
 import scipy as sc
 
 
-def matrix_multiplication(matrix_a, matrix_b):
-    """
-    Задание 1. Функция для перемножения матриц с помощью списков и циклов.
-    Вернуть нужно матрицу в формате списка.
-    """
-    # put your code here
-    pass
+def matrix_multiplication(a, b):
+    if len(a[0]) != len(b):
+        raise ValueError
+    result = []
+    for i in range(len(a)):
+        row = []
+        for j in range(len(b[0])):
+            s = 0
+            for k in range(len(b)):
+                s += a[i][k] * b[k][j]
+            row.append(s)
+        result.append(row)
+    return result
 
-
-def functions(a_1, a_2):
-    """
-    Задание 2. На вход поступает две строки, содержащие коэффициенты двух функций.
-    Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
-    Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
-    """
-    # put your code here
-    pass
-
+def functions(a, b):
+    c1 = list(map(float, a.split()))
+    c2 = list(map(float, b.split()))
+    if c1 == c2:
+        return None
+    common = []
+    for coeffs in [c1, c2]:
+        a, b, c = coeffs
+        discriminant = b**2 - 4 * a * c
+        if discriminant == 0:
+            root = -b / (2 * a)
+            common.append((root, a * root**2 + b * root + c))
+        elif discriminant > 0:
+            root1 = (-b + discriminant**0.5) / (2 * a)
+            root2 = (-b - discriminant**0.5) / (2 * a)
+            common.append((root1, a * root1**2 + b * root1 + c))
+            common.append((root2, a * root2**2 + b * root2 + c))
+    return common
 
 def skew(x):
-    """
-    Задание 3. Функция для расчета коэффициента асимметрии.
-    Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
-
+    n = len(x)
+    mean_x = sum(x) / n
+    m3 = 0
+    for xi in x:
+        m3 += (xi - mean_x)**3
+    m3 /= n
+    m2 = 0
+    for xi in x:
+        m2 += (xi - mean_x)**2
+    m2 /= n
+    sigma = m2**0.5
+    return round(m3 / sigma**3, 2)
 
 def kurtosis(x):
-    """
-    Задание 3. Функция для расчета коэффициента эксцесса.
-    Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
+    n = len(x)
+    mean_x = sum(x) / n
+    m4 = 0
+    for xi in x:
+        m4 += (xi - mean_x)**4
+    m4 /= n
+    m2 = 0
+    for xi in x:
+        m2 += (xi - mean_x)**2
+    m2 /= n
+    sigma = m2**0.5
+    return round(m4 / sigma**4 - 3, 2)
